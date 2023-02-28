72fee9d226b126b4211ff0f0c99735fd74245c27
Remi RAULT <remi.rault@ynov.com>
2023-02-28 17:37:19 +0100

    first test

--- yahtzee.test.js
+++ yahtzee.test.js
@@ -6,7 +6,7 @@ const isYahtzee = require('./yahtzee');
 describe("calculerScore", () => {
     const des = [1, 2, 3, 4, 5];
   
-    it("calcule le score pour la combinaison 'un'", () => {
+    it("calcule le score pour la combinaison 'un' !", () => {
       expect(calculerScore(des, "un")).toBe(1);
     });
 });
