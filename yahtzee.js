aa740a34ee574f08bfd875c6605ce13e169fe6ca
Remi RAULT <remi.rault@ynov.com>
2023-02-28 20:35:11 +0100

    Restructuatiion programme et test des fonction de jeu

--- yahtzee.js
+++ yahtzee.js
@@ -1,121 +1,67 @@
+// yahtzee.js
+
+// Fonction pour lancer les dés
 function lancerDes() {
-    let des = [];
-    for (let i = 0; i < 5; i++) {
-      des.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return des;
-  }
-  
-  // Calcul combinaison
-  function calculerScore(des, combinaison) {
-    let score = 0;
-    switch (combinaison) {
-      case "un":
-        score = des.filter((d) => d === 1).reduce((acc, d) => acc + d, 0);
-        break;
-      case "deux":
-        score = des.filter((d) => d === 2).reduce((acc, d) => acc + d, 0);
-        break;
-      case "trois":
-        score = des.filter((d) => d === 3).reduce((acc, d) => acc + d, 0);
-        break;
-      case "quatre":
-        score = des.filter((d) => d === 4).reduce((acc, d) => acc + d, 0);
-        break;
-      case "cinq":
-        score = des.filter((d) => d === 5).reduce((acc, d) => acc + d, 0);
-        break;
-      case "six":
-        score = des.filter((d) => d === 6).reduce((acc, d) => acc + d, 0);
-        break;
-      case "brelan":
-        for (let i = 1; i <= 6; i++) {
-          if (des.filter((d) => d === i).length >= 3) {
-            score = des.reduce((acc, d) => acc + d, 0);
-            break;
-          }
-        }
-        break;
-      case "carre":
-        for (let i = 1; i <= 6; i++) {
-          if (des.filter((d) => d === i).length >= 4) {
-            score = des.reduce((acc, d) => acc + d, 0);
-            break;
-          }
-        }
-        break;
-      case "full":
-        let freq = {};
-        des.forEach((d) => {
-          freq[d] = (freq[d] || 0) + 1;
-        });
-        if (Object.values(freq).includes(3) && Object.values(freq).includes(2)) {
-          score = 25;
-        }
-        break;
-      case "petite_suite":
-        if (
-          (des.includes(1) &&
-            des.includes(2) &&
-            des.includes(3) &&
-            des.includes(4)) ||
-          (des.includes(2) &&
-            des.includes(3) &&
-            des.includes(4) &&
-            des.includes(5)) ||
-          (des.includes(3) &&
-            des.includes(4) &&
-            des.includes(5) &&
-            des.includes(6))
-        ) {
-          score = 30;
-        }
-        break;
-      case "grande_suite":
-        if (
-          des.includes(1) &&
-          des.includes(2) &&
-          des.includes(3) &&
-          des.includes(4) &&
-          des.includes(5)
-        ) {
-          score = 40;
-        } else if (
-          des.includes(2) &&
-          des.includes(3) &&
-          des.includes(4) &&
-          des.includes(5) &&
-          des.includes(6)
-        ) {
-            score = 40;
-          }
-        break;
-     case "chance":
-    score = calculateChance(des);
-    break;
-    case "yahtzee":
-    if (isYahtzee(des)) {
-        score = 50;
-    }
-    break;
-    
-    
+  return Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1);
+}
+
+// Fonction pour trier les dés par ordre croissant
+function trierDes(des) {
+  return des.sort((a, b) => a - b);
+}
+
+// Fonction pour compter le nombre de dés ayant une certaine valeur
+function compterDes(des, valeur) {
+  return des.filter(d => d === valeur).length;
+}
+
+// Fonction pour vérifier si une combinaison est valide
+function estValide(des, combinaison) {
+  switch (combinaison) {
+    case 'brelan':
+      return des.some(d => compterDes(des, d) >= 3);
+    case 'carre':
+      return des.some(d => compterDes(des, d) >= 4);
+    case 'full':
+      return des.some(d => compterDes(des, d) === 3) &&
+             des.some(d => compterDes(des, d) === 2);
+    case 'petiteSuite':
+      return trierDes(des).join('') === '12345' ||
+             trierDes(des).join('') === '23456';
+    case 'grandeSuite':
+      return trierDes(des).join('') === '12345' ||
+             trierDes(des).join('') === '23456';
+    case 'yahtzee':
+      return des.every(d => d === des[0]);
     default:
-    score = 0;
-    break;
-    }
+      return true;
   }
+}
 
-function calculateChance(dice) {
-    return dice.reduce((sum, value) => sum + value, 0);
+// Fonction pour calculer le score pour une combinaison
+function calculerScore(des, combinaison) {
+  switch (combinaison) {
+    case 'brelan':
+      return des.reduce((acc, d) => acc + d, 0);
+    case 'carre':
+      return des.reduce((acc, d) => acc + d, 0);
+    case 'full':
+      return 25;
+    case 'petiteSuite':
+      return 30;
+    case 'grandeSuite':
+      return 40;
+    case 'yahtzee':
+      return 50;
+    default:
+      return des.reduce((acc, d) => acc + d, 0);
   }
+}
 
-  function isYahtzee(dice) {
-    const uniqueValues = new Set(dice);
-    return uniqueValues.size === 1;
-  }  
-
-  module.exports = lancerDes;
-  module.exports = calculerScore;
-  module.exports = calculateChance
-  module.exports = isYahtzee
+module.exports = {
+  lancerDes,
+  trierDes,
+  compterDes,
+  estValide,
+  calculerScore,
+};
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -1,12 +1,46 @@
-const lancerDes = require('./yahtzee');
-const calculerScore = require('./yahtzee');
-const calculateChance = require('./yahtzee');
-const isYahtzee = require('./yahtzee');
+// yams.test.js
 
-describe("calculerScore", () => {
-    const des = [1, 2, 3, 4, 5];
-  
-    it("calcule le score pour la combinaison 'un'", () => {
-      expect(calculerScore(des, "un")).toBe(1);
-    });
+const { lancerDes, trierDes, compterDes, estValide, calculerScore } = require('./yahtzee');
+
+describe('lancerDes', () => {
+  test('renvoie un tableau de 5 nombres', () => {
+    const des = lancerDes();
+    expect(des.length).toBe(5);
+    expect(des.every(d => typeof d === 'number')).toBe(true);
+  });
+});
+
+describe('trierDes', () => {
+  test('renvoie un tableau trié par ordre croissant', () => {
+    const des = [3, 1, 4, 5, 2];
+    const desTries = trierDes(des);
+    expect(desTries).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+describe('compterDes', () => {
+  test('renvoie le nombre de dés ayant une certaine valeur', () => {
+    const des = [3, 1, 4, 5, 3];
+    expect(compterDes(des, 3)).toBe(2);
+    expect(compterDes(des, 2)).toBe(0);
+  });
+});
+
+describe('estValide', () => {
+  test('renvoie true si la combinaison est valide', () => {
+    const des = [3, 3, 3, 5, 2];
+    expect(estValide(des, 'brelan')).toBe(true);
+  });
+
+  test('renvoie false si la combinaison n\'est pas valide', () => {
+    const des = [3, 3, 5, 5, 2];
+    expect(estValide(des, 'brelan')).toBe(false);
+  });
+});
+
+describe('calculerScore', () => {
+  test('renvoie le score pour une combinaison', () => {
+    const des = [3, 3, 3, 5, 2];
+    expect(calculerScore(des, 'brelan')).toBe(14);
+  });
 });
